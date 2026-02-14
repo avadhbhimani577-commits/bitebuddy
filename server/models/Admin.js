266e41da063e7c4a755a3ad4d833a32304000ceb
avadhbhimani577-commits <avadhbhimani577@gmail.com>
2026-02-14 15:13:56 +0530

    Create MongoDB schema for Admin user

---
+++ server/models/Admin.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+const adminSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    isAdmin: {
+        type: Boolean,
+        default: true,
+    },
+}, { timestamps: true });
+
+const Admin = mongoose.model('Admin', adminSchema);
+
+module.exports = Admin;
